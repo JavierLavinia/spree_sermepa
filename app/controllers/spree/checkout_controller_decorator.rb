368a923302ef1f3159a9027f67de29cad12dc982
Alvaro Sanchez <alvaro.sanchez@the-cocktail.com>
2013-06-24 10:30:44 +0200

    cambios flash[:alert] por :notice

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -38,7 +38,7 @@ module Spree
       load_order
       order_upgrade()
       payment_upgrade()
-      redirect_to completion_route, :alert => I18n.t(:order_processed_successfully)
+      redirect_to completion_route, :notice => I18n.t(:order_processed_successfully)
     end
 
     # create the gateway from the supplied options
