3afb0755cc3d8450295ea527e6fe8967a95af8ca
Ruben Gonzalez <ruben@fanatikcoders.com>
2017-05-30 18:07:37 +0200

    Fix super class, and accessible attrs for Spree::RedsysNotification

--- app/models/spree/redsys_notification.rb
+++ app/models/spree/redsys_notification.rb
@@ -1,5 +1,5 @@
-class Spree::RedsysNotification < Spree::Base
+class Spree::RedsysNotification < ActiveRecord::Base
   self.table_name = 'spree_redsys_notifications'
 
-
+  attr_accessible :order_id, :order_amount, :ds_amount, :ds_response, :ds_merchant_parameters, :response_code
 end
